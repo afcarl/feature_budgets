6c2e26e6e69371cf9cf74b4286b773f9ed26e298
Wesley Tansey <wes.tansey@gmail.com>
2014-04-29 20:02:50 -0500

    Fixed condor script

--- make_condor_jobs.py
+++ make_condor_jobs.py
@@ -30,7 +30,7 @@ Requirements = Precise
 """)
 
 job = """Log = {0}/condor_logs/{1}.log
-Arguments = skew_agents.py avg max ucb-avg ucb-max --features 50 --values_per_feature 2 --classes 3 --instances 100 --data_nodes 40 --steps 1 --acquisitions_per_step 3 --sparsity 0.5 --trials 1 --feature_bias 0.6 --class_bias 0.6 --outfile {0}/results/{1}.csv --max_optional_features 2 --max_tree_counts 25 50 100 200 500 1000 2000
+Arguments = test_scoring.py avg max ucb-avg ucb-max --features 50 --values_per_feature 2 --classes 3 --instances 100 --data_nodes 40 --steps 1 --acquisitions_per_step 3 --sparsity 0.5 --trials 1 --feature_bias 0.6 --class_bias 0.6 --outfile {0}/results/{1}.csv --max_optional_features 2 --max_tree_counts 25 50 100 200 500 1000 2000
 Output = {0}/output/{1}.out
 Error = {0}/error/{1}.log
 Queue 1
@@ -40,7 +40,7 @@ Queue 1
 
 for job_id in xrange(NUM_JOBS):
     f.write(job.format(experiment_dir, job_id))
-    
+
 f.flush()
 f.close()
 
